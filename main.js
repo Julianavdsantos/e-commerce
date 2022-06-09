7760140c8ccbd56733c39cbc7413bb31f43c1f50
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2022-06-09 00:22:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+const btnvar1 = document.getElementById('btnh1');
+function Toggle1(){
+  if (btnvar1.style.color =="red") {
+      btnvar1.style.color = "grey"
+  }
+  else{
+      btnvar1.style.color = "red"
+  }
+}
+
+
+const btnvar2 = document.getElementById('btnh2');
+function Toggle2(){
+  if (btnvar2.style.color =="red") {
+      btnvar2.style.color = "grey"
+  }
+  else{
+      btnvar2.style.color = "red"
+  }
+}
+
+const btnvar3 = document.getElementById('btnh3');
+function Toggle3(){
+  if (btnvar3.style.color =="red") {
+      btnvar3.style.color = "grey"
+  }
+  else{
+      btnvar3.style.color = "red"
+  }
+}
